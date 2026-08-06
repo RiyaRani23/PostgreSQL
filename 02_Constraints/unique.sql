f838b4ce34e393dfbd2cd4ff5d69a44fd72a50bb
Riya Rani <riyarani1404@gmail.com>
2026-08-06 22:32:33 +0530

    Add users table with unique constraints
    Created a users table with unique constraints on username and email.

--- 02_Constraints/unique.sql
+++ 02_Constraints/unique.sql
@@ -1,1 +1,14 @@
+-- UNIQUE Constraint
 
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE,
+    email VARCHAR(100) UNIQUE
+);
+
+INSERT INTO users(username,email)
+VALUES
+('aarav01','aarav@gmail.com'),
+('ananya02','ananya@gmail.com');
+
+SELECT * FROM users;
