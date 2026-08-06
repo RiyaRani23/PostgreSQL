a62084e31f8ef24cf2e95aef2bc6f804f93ba894
Riya Rani <riyarani1404@gmail.com>
2026-08-06 22:33:13 +0530

    Add NOT NULL constraint to employees table

--- 02_Constraints/not_null.sql
+++ 02_Constraints/not_null.sql
@@ -1,1 +1,14 @@
+-- NOT NULL Constraint
 
+CREATE TABLE employees(
+    employee_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    salary NUMERIC(10,2) NOT NULL
+);
+
+INSERT INTO employees(name,salary)
+VALUES
+('Rohan',45000),
+('Sneha',55000);
+
+SELECT * FROM employees;
