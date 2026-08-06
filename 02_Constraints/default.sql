6b9ab8e550c9bb4233d08e828bd1f8433a681922
Riya Rani <riyarani1404@gmail.com>
2026-08-06 22:33:40 +0530

    Add orders table with DEFAULT constraints

--- 02_Constraints/default.sql
+++ 02_Constraints/default.sql
@@ -1,1 +1,17 @@
 
+-- DEFAULT Constraint
+
+CREATE TABLE orders(
+    order_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(100),
+    quantity INT DEFAULT 1,
+    order_date DATE DEFAULT CURRENT_DATE
+);
+
+INSERT INTO orders(product_name)
+VALUES ('Laptop');
+
+INSERT INTO orders(product_name, quantity)
+VALUES ('Keyboard',2);
+
+SELECT * FROM orders;
