73a1bf1a1e30871c3645bc78e09a42978b077c8b
Riya Rani <riyarani1404@gmail.com>
2026-08-06 22:34:07 +0530

    Create students table with age CHECK constraint
    Added a CHECK constraint to ensure students are at least 18 years old.

--- 02_Constraints/check.sql
+++ 02_Constraints/check.sql
@@ -1,1 +1,14 @@
+-- CHECK Constraint
 
+CREATE TABLE students(
+    student_id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    age INT CHECK(age >= 18)
+);
+
+INSERT INTO students(name, age)
+VALUES
+('Aarav',20),
+('Rahul',21);
+
+SELECT * FROM students;
