22e79da1ecf14e9a0d97ecde06bce9b1fdecb85c
Riya Rani <riyarani1404@gmail.com>
2026-08-06 22:29:04 +0530

    Add primary key constraint to students table

--- 02_Constraints/primary_key.sql
+++ 02_Constraints/primary_key.sql
@@ -1,1 +1,15 @@
 
+-- PRIMARY KEY Constraint
+
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    age INT
+);
+
+INSERT INTO students(name, age)
+VALUES
+('Aarav',20),
+('Ananya',19);
+
+SELECT * FROM students;
