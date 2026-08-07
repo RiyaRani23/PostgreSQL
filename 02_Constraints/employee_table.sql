e6b9a535111786e8a4795bd16ec33e5bdae1a305
Riya Rani <riyarani1404@gmail.com>
2026-08-07 21:14:16 +0530

    Add multiple employee records to employee_table

--- 02_Constraints/employee_table.sql
+++ 02_Constraints/employee_table.sql
@@ -7,3 +7,44 @@ CREATE TABLE employee (
     salary DECIMAL(10, 2) DEFAULT 30000.00,
     hire_date DATE NOT NULL DEFAULT CURRENT_DATE
 );
+
+INSERT INTO employee
+VALUES (
+    DEFAULT,
+    'Aarav',
+    'Kumar',
+    'aarav@gmail.com',
+    'IT',
+    56000,
+    DEFAULT
+);
+
+INSERT INTO employee
+VALUES (DEFAULT, 'Riya', 'Sharma', 'riya@gmail.com', 'HR', 45000, DEFAULT);
+
+INSERT INTO employee
+VALUES (DEFAULT, 'Rahul', 'Verma', 'rahul@gmail.com', 'IT', 60000, DEFAULT);
+
+INSERT INTO employee
+VALUES (DEFAULT, 'Priya', 'Singh', 'priya@gmail.com', 'Finance', 52000, DEFAULT);
+
+INSERT INTO employee
+VALUES (DEFAULT, 'Aman', 'Kumar', 'aman@gmail.com', 'IT', 58000, DEFAULT);
+
+INSERT INTO employee
+VALUES (DEFAULT, 'Neha', 'Gupta', 'neha@gmail.com', 'HR', 47000, DEFAULT);
+
+INSERT INTO employee
+VALUES (DEFAULT, 'Rohit', 'Mehta', 'rohit@gmail.com', 'Sales', 50000, DEFAULT);
+
+INSERT INTO employee
+VALUES (DEFAULT, 'Ananya', 'Das', 'ananya@gmail.com', 'Finance', 55000, DEFAULT);
+
+INSERT INTO employee
+VALUES (DEFAULT, 'Vikas', 'Yadav', 'vikas@gmail.com', 'Sales', 48000, DEFAULT);
+
+INSERT INTO employee
+VALUES (DEFAULT, 'Sneha', 'Roy', 'sneha@gmail.com', 'IT', 62000, DEFAULT);
+
+INSERT INTO employee
+VALUES (DEFAULT, 'Karan', 'Malhotra', 'karan@gmail.com', 'Marketing', 51000, DEFAULT);
