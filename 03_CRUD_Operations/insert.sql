d3f839743ff325e2cc4d7be838533b62c96180d1
Riya Rani <riyarani1404@gmail.com>
2026-08-08 08:15:33 +0530

    Add SQL insert statements for employees
    Added SQL commands to insert one employee and multiple employees into the employee table.

---
+++ 03_CRUD_Operations/insert.sql
@@ -0,0 +1,18 @@
+-- Insert one employee
+INSERT INTO employee
+VALUES (
+    DEFAULT,
+    'Aarav',
+    'Kumar',
+    'aarav@gmail.com',
+    'IT',
+    56000,
+    DEFAULT
+);
+
+-- Insert multiple employees
+INSERT INTO employee
+VALUES
+(DEFAULT, 'Riya', 'Sharma', 'riya@gmail.com', 'HR', 45000, DEFAULT),
+(DEFAULT, 'Rahul', 'Verma', 'rahul@gmail.com', 'IT', 60000, DEFAULT),
+(DEFAULT, 'Priya', 'Singh', 'priya@gmail.com', 'Finance', 52000, DEFAULT);
