e1d71eb6c295da305fcc43c831c4f1e61fcdbe7b
Riya Rani <riyarani1404@gmail.com>
2026-08-08 09:07:27 +0530

    Add aggregate functions examples in SQL
    Added SQL queries demonstrating aggregate functions including COUNT, SUM, AVG, MIN, and MAX.

---
+++ 06_Aggregate_Functions/aggregate_functions.sql
@@ -0,0 +1,27 @@
+-- COUNT
+SELECT COUNT(*)
+FROM employee;
+
+-- COUNT specific column
+SELECT COUNT(email)
+FROM employee;
+
+
+-- SUM
+SELECT SUM(salary)
+FROM employee;
+
+
+-- AVG
+SELECT AVG(salary)
+FROM employee;
+
+
+-- MIN
+SELECT MIN(salary)
+FROM employee;
+
+
+-- MAX
+SELECT MAX(salary)
+FROM employee;
