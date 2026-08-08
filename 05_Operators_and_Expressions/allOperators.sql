4a0ce0ef02898ab1cb6b3205980a7f74bf81714b
Riya Rani <riyarani1404@gmail.com>
2026-08-08 08:31:52 +0530

    Add SQL operator examples and explanations
    Added examples of SQL operators including DISTINCT and LIKE with explanations.

--- 05_Operators_and_Expressions/allOperators.sql
+++ 05_Operators_and_Expressions/allOperators.sql
@@ -1,1 +1,19 @@
+-- DISTINCT -> Used to remove duplicate results:
 
+SELECT DISTINCT dept
+FROM employee;
+
+-- LIKE -> Used for pattern matching:
+
+SELECT fName
+FROM employee
+WHERE fName LIKE 'A%';
+
+-- This finds names starting with A.
+
+-- Some useful patterns:
+
+'A%'    -- starts with A
+'%a'    -- ends with a
+'%ar%'  -- contains "ar"
+'_a%'   -- second character is a
