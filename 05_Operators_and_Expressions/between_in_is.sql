dffb16ed2415e3ee87f41d2d4cd81e59e10272f6
Riya Rani <riyarani1404@gmail.com>
2026-08-08 08:37:57 +0530

    Add SQL examples for BETWEEN, IN, and IS NULL
    Added SQL queries demonstrating the use of BETWEEN, IN, and IS NULL operators.

--- 05_Operators_and_Expressions/between_in_is.sql
+++ 05_Operators_and_Expressions/between_in_is.sql
@@ -1,1 +1,72 @@
+-- between.sql
+-- Employees whose salary is between 50000 and 60000
+SELECT *
+FROM employee
+WHERE salary BETWEEN 50000 AND 60000;
 
+-- Employees hired between two dates
+SELECT *
+FROM employee
+WHERE hire_date BETWEEN '2026-01-01' AND '2026-12-31';
+in.sql
+-- ************************************
+-- IN checks whether a value matches any value in a list.
+
+-- Employees from IT or HR
+SELECT *
+FROM employee
+WHERE dept IN ('IT', 'HR');
+
+-- Employees with specific salaries
+SELECT fName, salary
+FROM employee
+WHERE salary IN (45000, 50000, 60000);
+is_null.sql
+
+  -- *************************************************
+-- Used to check NULL values.
+
+-- Employees whose department is NULL
+SELECT *
+FROM employee
+WHERE dept IS NULL;
+
+-- Employees whose department is NOT NULL
+SELECT *
+FROM employee
+WHERE dept IS NOT NULL;
+
+-- Important: Don't use:
+
+-- WHERE dept = NULL;
+
+-- Use:
+
+WHERE dept IS NULL;
+
+-- *********************************
+-- comparison_operators.sql
+
+-- Equal to
+SELECT * FROM employee
+WHERE salary = 50000;
+
+-- Not equal to
+SELECT * FROM employee
+WHERE salary <> 50000;
+
+-- Greater than
+SELECT * FROM employee
+WHERE salary > 50000;
+
+-- Less than
+SELECT * FROM employee
+WHERE salary < 50000;
+
+-- Greater than or equal to
+SELECT * FROM employee
+WHERE salary >= 50000;
+
+-- Less than or equal to
+SELECT * FROM employee
+WHERE salary <= 50000;
